a8a9768573cd2e0f8c58b542c07bbc7f11b2904c
VnssJT <64807616+VnssJT@users.noreply.github.com>
2021-10-05 20:22:09 +0200

    cambio de los controllers para poder hacer merge

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,79 +4,97 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-  public float speed = 3.0f;
-
-  public int maxHealth = 5;
-  public float timeInvincible = 2.0f;
-
-  public int health {get {return currentHealth;}}
-  int currentHealth;
-
-  bool isInvincible;
-  float invincibleTimer;
-  
-  Rigidbody2D rigidbody2d;
-  float horizontal;
-  float vertical;
-
-
-  Animator animator;
-  Vector2 lookDirection = new Vector2(1,0);
-
+    public float speed = 3.0f;
+    
+    public int maxHealth = 5;
+    
+    public GameObject projectilePrefab;
+    
+    public int health { get { return currentHealth; }}
+    int currentHealth;
+    
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+    
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+    
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-
         animator = GetComponent<Animator>();
-        currentHealth = maxHealth;    
-      
+        
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-      horizontal = Input.GetAxis("Horizontal");
-      vertical = Input.GetAxis("Vertical");             //El personaje es frame independent
-
-      Vector2 move = new Vector2(horizontal, vertical);
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
         
-      if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-      {
-        lookDirection.Set(move.x, move.y);
-        lookDirection.Normalize();
-      }
+        Vector2 move = new Vector2(horizontal, vertical);
         
-      animator.SetFloat("Look X", lookDirection.x);
-      animator.SetFloat("Look Y", lookDirection.y);
-      animator.SetFloat("Speed", move.magnitude);
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+        
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+        
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+    }
+    
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
-      if(isInvincible){
-        invincibleTimer -= Time.deltaTime;
-        if(invincibleTimer < 0)
-          isInvincible = false;
-      }
+        rigidbody2d.MovePosition(position);
     }
 
-    void FixedUpdate(){
-      Vector2 position = rigidbody2d.position;
-      position.x = position.x + speed * horizontal * Time.deltaTime;
-      position.y = position.y + speed * vertical * Time.deltaTime;  
-      rigidbody2d.MovePosition(position);
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
+    
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
 
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
 
-    public void ChangeHealth(int amount){
-      if(amount < 0){
-        animator.SetTrigger("Hit");
-        if(isInvincible) 
-          return;
-        isInvincible = true;
-        invincibleTimer = timeInvincible;
-      }
-
-      currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-      //Debug.Log("LIFE : " + currentHealth + "/" + maxHealth);
-      UIHealthBar.instance.SetValue(currentHealth/ (float)maxHealth);
+        animator.SetTrigger("Launch");
     }
 }
